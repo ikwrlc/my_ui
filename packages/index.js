4317424875d2fdf40402a8b2437e802c56bbcd2d
lcwr川 <381451067@qq.com>
2023-08-11 17:09:32 +0800

    :rocket: project init

--- packages/index.js
+++ packages/index.js
@@ -10,6 +10,7 @@ const components = [
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，那么所有的组件都会被注册
 const install = Vue=>{
     // 判断是否安装
+    
     if (install.installed) return
     // 遍历注册全局组件
     components.map(component => Vue.component(component.name, component))
